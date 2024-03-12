33856a2ae458f2ec29d5cdbdeb61c78660905967
Danik <bravedanik@gmail.com>
2024-03-12 09:23:29 +0300

    JavaProject

--- src/main/java/org/example/javalab/repository/EmailRepository.java
+++ src/main/java/org/example/javalab/repository/EmailRepository.java
@@ -2,10 +2,9 @@ package org.example.javalab.repository;
 
 import org.example.javalab.entity.Email;
 import org.example.javalab.entity.EmailType;
-import org.example.javalab.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 import java.util.Set;
 
 public interface EmailRepository extends JpaRepository<Email,Long> {
--- src/main/java/org/example/javalab/repository/RequestRepository.java
+++ src/main/java/org/example/javalab/repository/RequestRepository.java
@@ -1,10 +1,8 @@
 package org.example.javalab.repository;
 
-import org.example.javalab.entity.Email;
 import org.example.javalab.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
 
 public interface RequestRepository  extends JpaRepository<Request,Long> {
     Request findByRequestText(String requestText);
