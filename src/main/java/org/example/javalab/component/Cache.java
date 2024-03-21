a9c14079c20df7dd3b18dcbfb9cc8f48d7e17783
Danik <bravedanik@gmail.com>
2024-03-21 16:33:13 +0300

    JavaProject

--- src/main/java/org/example/javalab/component/Cache.java
+++ src/main/java/org/example/javalab/component/Cache.java
@@ -8,11 +8,15 @@ import java.util.Map;
 @Component
 public class Cache {
     private final Map<String,Object> hashMap;
+    private final int maxSize = 20;
 
     public Cache() {
         this.hashMap = new HashMap<>();
     }
     public void put(String key, Object value){
+        if(hashMap.size()==maxSize){
+            removeOldestKey();
+        }
         hashMap.put(key, value);
     }
     public Object get(String key){
@@ -27,5 +31,15 @@ public class Cache {
     public void clear(){
         hashMap.clear();
     }
-
+    private void removeOldestKey(){
+        String oldestKey = null;
+        for(String key : hashMap.keySet()){
+            if(oldestKey==null||oldestKey.hashCode()< oldestKey.hashCode()){
+                oldestKey=key;
+            }
+        }
+        if(oldestKey!=null){
+            hashMap.remove(oldestKey);
+        }
+    }
 }
